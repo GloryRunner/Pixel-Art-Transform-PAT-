d66ad38197b783b44144711f9a16ef6a4f23473b
Glory005 <89966094+Glory005@users.noreply.github.com>
2023-06-29 14:51:54 -0700

    Matrices

---
+++ src/ReplicatedFirst/Matrices.lua
@@ -0,0 +1,17 @@
+local Matrices = {}
+
+function Matrices.new()
+
+end
+
+function Matrices:Transpose()
+    
+end
+
+function Matrices:Append()
+    
+end
+
+function Matrices:Orthonormalize()
+    
+end
